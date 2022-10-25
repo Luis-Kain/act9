e3efda9942523a0d69544991dae0fb0462a4841c
Luis Kain <luiskainlozano@gmail.com>
2022-10-25 14:09:04 -0500

    se añadio manejo de exepciones

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.println("Lista de opciones\n1 Mezclar deck\n2 Sacar una carta\n3 carta al azar" +
                 "\n4 Generar una mano de 5 cartas\n0 salir");
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ArrayList<Card> myHand = new ArrayList<>();
         Deck myDeck = new Deck();
         Scanner sc =  new Scanner(System.in);
@@ -24,23 +24,34 @@ public class Main {
             System.out.printf("Inserte un número del 1 al 4 para elegír la función o 0 para salir del programa:");
             switch (sc.nextInt()){
                 case 1:
+                    if(myDeck.getMyCards().size() < 1){
+                        throw new Exception("No hay suficientes cartas");
+                    }
                     myDeck.shuffle();
                     break;
                 case 2:
+                    if(myDeck.getMyCards().size() < 1){
+                        throw new Exception("No hay suficientes cartas");
+                    }
                     myDeck.head();
                     break;
                 case 3:
+                    if(myDeck.getMyCards().size() < 1){
+                        throw new Exception("No hay suficientes cartas");
+                    }
                     myDeck.pick();
                     break;
                 case 4:
+                    if(myDeck.getMyCards().size() < 5){
+                        throw new Exception("No hay suficientes cartas");
+                    }
                     myDeck.hand(myHand);
                     break;
                 case 0:
                     isPlaying = false;
                     break;
                 default:
-                    System.out.println("opción invalida, solo números del 0 al 4");
-                    showMenu();
+                    throw new Exception("Opción no valida");
             }
 
         }while(isPlaying);
